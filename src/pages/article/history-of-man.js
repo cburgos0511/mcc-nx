b5f80410e656c5f6764f3ab4639acb83dcc757af
Cruz Burgos <cburgos@nodecentric.com>
2020-12-12 10:48:36 -0600

    commented out history-of-man

--- src/pages/article/history-of-man.js
+++ src/pages/article/history-of-man.js
@@ -1,39 +1,39 @@
-import HM from '../../assets/History_of_Man.pdf'
-import React, { useState } from 'react'
-import Timeline from '../../components/Articles/Timeline'
+// import HM from '../../assets/History_of_Man.pdf'
+// import React, { useState } from 'react'
+// import Timeline from '../../components/Articles/Timeline'
 
-const HistoryOfMan = () => {
-    const [numPages, setNumPages] = useState(null)
-    const [pageNumber, setPageNumber] = useState(1)
+// const HistoryOfMan = () => {
+//     const [numPages, setNumPages] = useState(null)
+//     const [pageNumber, setPageNumber] = useState(1)
 
-    const onDocumentLoadSuccess = ({ numPages }) => {
-        setNumPages(numPages)
-    }
+//     const onDocumentLoadSuccess = ({ numPages }) => {
+//         setNumPages(numPages)
+//     }
 
-    const changePage = offset => {
-        setPageNumber(prevPageNumber => prevPageNumber + offset)
-    }
+//     const changePage = offset => {
+//         setPageNumber(prevPageNumber => prevPageNumber + offset)
+//     }
 
-    const previousPage = () => {
-        changePage(-1)
-    }
+//     const previousPage = () => {
+//         changePage(-1)
+//     }
 
-    const nextPage = () => {
-        changePage(1)
-    }
-    return (
-        <main>
-            <Timeline
-                title="History of Man"
-                load={onDocumentLoadSuccess}
-                previousPage={previousPage}
-                nextPage={nextPage}
-                currentPage={pageNumber}
-                totalPages={numPages}
-                pdf={HM}
-            />
-        </main>
-    )
-}
+//     const nextPage = () => {
+//         changePage(1)
+//     }
+//     return (
+//         <main>
+//             <Timeline
+//                 title="History of Man"
+//                 load={onDocumentLoadSuccess}
+//                 previousPage={previousPage}
+//                 nextPage={nextPage}
+//                 currentPage={pageNumber}
+//                 totalPages={numPages}
+//                 pdf={HM}
+//             />
+//         </main>
+//     )
+// }
 
-export default HistoryOfMan
+// export default HistoryOfMan
