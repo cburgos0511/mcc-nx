19503a7ee24ad0061cbe9baaf4fb8516d1c2fa5c
Cruz Burgos <cburgos@nodecentric.com>
2021-03-23 12:07:52 -0500

    Removed Random stuff

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Header from "../components/Contacts/Header";
 import Map from "../components/Contacts/Map";
 import ContactForm from "../components/Contacts/ContactForm";
@@ -7,6 +8,9 @@ import s from "../components/Contacts/contact.module.scss";
 const contact = () => {
   return (
     <>
+      <Head>
+        <title>Contact</title>
+      </Head>
       <Header />
       <div className={s.contactContainer}>
         <Map />
--- src/utils/fetcher.js
+++
@@ -1,7 +0,0 @@
-const fetcher = (...args) =>
-  fetch(...args).then(async (r) => {
-    if (r.ok) return r.json();
-    return [];
-  });
-
-export default fetcher;
