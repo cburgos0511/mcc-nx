3e9f9046e30eef272a71c9c1f74042fa0f69f29c
Jerod Santo <jerod.santo@gmail.com>
2023-12-22 10:09:42 -0600

    Add two new resources

--- src/data/resources.js
+++ src/data/resources.js
@@ -19,6 +19,24 @@ const resources = [
     date_published: ""
   },
   {
+    id: "CC253BB1-96DC-4538-B44A-4175E9466ECE",
+    title: "Dispensationalism",
+    author: "Mark Mancuso",
+    description: "An explanation of the Bible's use of dispensations.",
+    link: "/Dispensationalism.pdf",
+    local: false,
+    date_published: ""
+  },
+  {
+    id: "E89FC2E1-3562-4981-A8FC-CEBF2AEB9155",
+    title: "Miracles in the New Testament",
+    author: "Mark Mancuso",
+    description: "The three words translated 'miracle' in the NT and what they mean.",
+    link: "/Miracles_in_the_New_Testament.pdf",
+    local: false,
+    date_published: ""
+  },
+  {
     id: "95269daa-6b03-4d9c-96e4-1f0b03032b9d",
     title: "The Earlier Years of the Modern Tongue Movement",
     author: "G.H. Lang",
